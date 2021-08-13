13cec99a6d1443d3cce11b255bd5f9667770fd3d
Sean-Y00 <bsyoo@myseneca.ca>
2021-08-13 19:44:28 -0400

    fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { from } from 'rxjs';
+
 import { AboutComponent } from './about/about.component';
+import { AlbumComponent } from './album/album.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NewReleasesComponent } from './new-releases/new-releases.component';
 import { ArtistDiscographyComponent } from './artist-discography/artist-discography.component';
-import { AlbumComponent } from './album/album.component';
 import { SearchResultComponent } from './search-result/search-result.component';
 import { FavouritesComponent } from './favourites/favourites.component';
-import { GuardAuthService } from './guard-auth.service';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
+import { GuardAuthService } from './guard-auth.service';
+
 const routes: Routes = [
-  { path: 'about', component: AboutComponent, canActivate: [GuardAuthService] },
-  {
-    path: 'newReleases',
-    component: NewReleasesComponent,
-    canActivate: [GuardAuthService],
-  },
   {
     path: 'artist/:id',
     component: ArtistDiscographyComponent,
@@ -29,17 +25,6 @@ const routes: Routes = [
     canActivate: [GuardAuthService],
   },
   {
-    path: 'search',
-    component: SearchResultComponent,
-    canActivate: [GuardAuthService],
-  },
-  { path: '', redirectTo: 'newReleases', pathMatch: 'full' },
-  {
-    path: 'favourites',
-    component: FavouritesComponent,
-    canActivate: [GuardAuthService],
-  },
-  {
     path: 'register',
     component: RegisterComponent,
   },
@@ -47,6 +32,32 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent,
   },
+  {
+    path: 'newRelease',
+    component: NewReleasesComponent,
+    canActivate: [GuardAuthService],
+  },
+  {
+    path: 'search',
+    component: SearchResultComponent,
+    canActivate: [GuardAuthService],
+  },
+  {
+    path: 'favourites',
+    component: FavouritesComponent,
+    canActivate: [GuardAuthService],
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    canActivate: [GuardAuthService],
+  },
+  {
+    path: '',
+    redirectTo: 'newRelease',
+    pathMatch: 'full',
+  },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
